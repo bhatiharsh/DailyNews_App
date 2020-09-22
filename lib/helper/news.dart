b9407a04a5f1a1faff0c3a05fe35377cd31a9296
BHATI369 <bhatisunny369@gmail.com>
2020-09-22 13:54:21 +0530

    My First Commit

---
+++ lib/helper/news.dart
@@ -0,0 +1,78 @@
+import 'package:http/http.dart' as http;
+import 'package:daily_news1/models/article.dart';
+import 'dart:convert';
+
+class News {
+
+  List<Article> news  = [];
+
+  Future<void> getNews() async{
+
+    String url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=d15b394582754fe89139ec24e94a4de7";
+
+    var response = await http.get(url);
+
+    var jsonData = jsonDecode(response.body);
+
+    if(jsonData['status'] == "ok"){
+      jsonData["articles"].forEach((element){
+
+        if(element['urlToImage'] != null && element['description'] != null){
+          Article article = Article(
+            title: element['title'],
+            author: element['author'],
+            description: element['description'],
+            urlToImage: element['urlToImage'],
+            publshedAt: DateTime.parse(element['publishedAt']),
+            content: element["content"],
+            articleUrl: element["url"],
+          );
+          news.add(article);
+        }
+
+      });
+    }
+
+
+  }
+
+
+}
+
+
+class NewsForCategorie {
+
+  List<Article> news  = [];
+
+  Future<void> getNewsForCategory(String category) async{
+
+    String url = "http://newsapi.org/v2/top-headlines?country=in&category=$category&apiKey=d15b394582754fe89139ec24e94a4de7";
+
+    var response = await http.get(url);
+
+    var jsonData = jsonDecode(response.body);
+
+    if(jsonData['status'] == "ok"){
+      jsonData["articles"].forEach((element){
+
+        if(element['urlToImage'] != null && element['description'] != null){
+          Article article = Article(
+            title: element['title'],
+            author: element['author'],
+            description: element['description'],
+            urlToImage: element['urlToImage'],
+            publshedAt: DateTime.parse(element['publishedAt']),
+            content: element["content"],
+            articleUrl: element["url"],
+          );
+          news.add(article);
+        }
+
+      });
+    }
+
+
+  }
+
+
+}
